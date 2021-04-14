9b7234bf31f35fa94ca3a7db6e10aa221420a05a
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-04-14 15:08:19 +0200

    add first js steps and few styles

--- js/script.js
+++ js/script.js
@@ -1,13 +1,64 @@
 $(document).ready(function(){
 
 
+    /**
+     * 
+     *  TODO LIST APP
+     *  
+     *  1. POPULATE TODO LIST
+     *  2. INSERT NEW TODO TO THE LIST
+     *  3. REMOVE TODO FROM THE LIST
+     *  4. CHECK IF THEY ARE ALREADY COMPLETED OR NOT
+     * 
+     * 
+    /**/
 
+    var toDoItems = [
+        {
+            text: 'buy some fruits',
+            completed: false,
+        },
+        {
+            text: 'buy some clothes',
+            completed: false,
+        },
+        {
+            text: 'pay the rents',
+            completed: true,
+        },
+        {
+            text: 'study javascript & jQuery',
+            completed: true,
+        },
+        {
+            text: 'go out for a walk to relax',
+            completed: false,
+        },
+    ];
 
+    // REFS
+    var list = $('.todos');
+    var newInput = $('.add-todo');
+    var template = $('#template li');
 
+    // 1.
 
+    for(var i = 0; i < toDoItems.length; i++) {
 
+        // template
+        var toDo = toDoItems[i];
+        var item = template.clone();
+        item.find('.text').text(toDo.text);
 
-
+        // check if completed
+        /* add later when styles will be done
+        if(toDo.completed) {
+            item.find('.text').toggleClass('completed');
+        }
+        */
+        // add to the list
+        list.append(item);
+    }
 
 
 
